b12dfafac36964bb925a71eb4389eb82780cd32d
舒克 <shuke.cl@taobao.com>
2013-06-19 11:16:03 +0800

    修复showMessage时定位的BUG

--- 1.0/build/index-min.js
+++ 1.0/build/index-min.js
@@ -1,3 +1,3 @@
-/*! autocomplete - v1.0 - 2013-06-18 3:00:50 PM
+/*! autocomplete - v1.0 - 2013-06-19 11:15:41 AM
 * Copyright (c) 2013 舒克; Licensed  */
 KISSY.add(function(a,b,c,d,e){return b.extend([c,d,e],{},{})},{requires:["rich-base","./base","./rich","./hot","./autocomplete.css"]});
